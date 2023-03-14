8c06c02c396d6e584a58a8547280e0bcc2739fa9
Maysun Campos <maysuncampos@yahoo.com>
2023-03-13 18:01:45 -0600

    Main file

---
+++ main.go
@@ -0,0 +1,9 @@
+package main;
+
+import fmt;
+
+func main() {
+  fmt.println("Please contact us for making packages");
+  fmt.println("once you contact us you must have your source code connected to the email");
+  fmt.println("we will review the code and add it if it fits our rules enjoy - FriendMyFriend");
+}
